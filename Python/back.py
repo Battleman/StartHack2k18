d5b1fac3b57741773182ca3c12735458556bb123
Olivier Cloux <olivier.cloux@epfl.ch>
2018-02-25 01:26:48 +0100

    Auto stash before merge of "master" and "origin/master"
    Really cool

--- Python/back.py
+++ Python/back.py
@@ -4,8 +4,10 @@ from urllib.request import urlopen
 def getInfo(url) :
     html = urlopen(url)
     soup = BeautifulSoup(html, 'html.parser')
+    media = ''
     
-    if ( 'foxnews' in url ) :
+    if ( 'foxnews.com' in url ) :
+        media = 'foxnews'
         
         date = soup.find('time')['data-time-published']
         articleHTML = soup.find('div', attrs={'class':'article-body'})
@@ -14,30 +16,128 @@ def getInfo(url) :
         ps = articleHTML.findAll('p')
         for p in ps :
             article += p.text +'\n'
-        
-    elif ('cnn' in url ) :
+    
+    elif ('cnn.com' in url ) :
+        media = 'cnn'
         date = soup.find('meta', attrs={'itemprop':'dateCreated'})['content']
         articleHTML = soup.find('div', attrs={'itemprop':'articleBody'})
         
+        #too random to be more clean
         article =articleHTML.text
+        
+        
+        
+    elif ('breitbart.com' in url ) :
+        media = 'breitbart'
+        date = soup.find('span', attrs={'class':'bydate'}).text
+        articleHTML = soup.find('div', attrs={'class':'entry-content'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+            
+    elif ('buzzfeed.com' in url ) :
+        
+        for div in soup.find_all('div', attrs={'data-module':'action-bar-pagelevel'}): 
+            div.decompose()
+        
+        soup.find('span', attrs={'class':'js-subbuzz__title-text'}).decompose()
+        
+        media = 'buzzfeed'
+        date = soup.find('time', attrs={'class':'buzz-timestamp__time js-timestamp__time'}).text
+        articleHTML = soup.find('article', attrs={'class':'buzz article article--long clearfix'})
+        
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+        
+     
+    elif ('breitbart.com' in url ) :
+        media = 'breitbart'
+        date = soup.find('span', attrs={'class':'bydate'}).text
+        articleHTML = soup.find('div', attrs={'class':'entry-content'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+            
+    elif ('20min.ch' in url ) :
+        media = '20min'
+        date = soup.find('div', attrs={'class':'published clearfix'}).find('span').text
+        articleHTML = soup.find('div', attrs={'class':'story_text'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+            
+    elif ('24heures.ch' in url ) :
+        media = '24heures'
+        date = soup.find('time', attrs={'class':'time'}).text
+        articleHTML = soup.find('div', attrs={'id':'mainContent'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+            
+    elif ('letemps.ch' in url ) :
+        media = 'letemps'
+        date = soup.find('meta', attrs={'itemprop':'datePublished'}).text
+        articleHTML = soup.find('div', attrs={'class':'body_content'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+            
+            
+    elif ('tdg.ch' in url ) :
+        media = 'tdg'
+        date = soup.find('time', attrs={'class':'time'}).text
+        articleHTML = soup.find('div', attrs={'id':'mainContent'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+    
+    elif ('legorafi.fr' in url ) :
+        media = 'legorafi'
+        date = soup.find('span', attrs={'class':'context'}).text.split(' ')[2]
+        articleHTML = soup.find('div', attrs={'class':'content'})
+        
+        article = ''
+        ps = articleHTML.findAll('p')
+        for p in ps :
+            article += p.text +'\n'
+        
      
     hrefs = articleHTML.findAll('a')    
-    sameHref = []
-    diffHrefs = []
+    sameHref = set()
+    diffHrefs = set()
     domainRef=set()
     
+    
+        
     for h in hrefs :
         newHref = h.get('href')
-        if newHref == None :
+        if newHref == None or len( newHref.split('/') )== 1 :
             continue
+            
         if 'http' in newHref :
             domainRef.add(newHref.split('/')[2])
         else :
             domainRef.add(newHref.split('/')[0])
             
-        if 'foxnews' in  newHref :
-            sameHref += [newHref]
+        if media in  newHref :
+            sameHref.add(newHref)
         else :
-            diffHrefs += [newHref]
+            diffHrefs.add(newHref)
+                    
     
-    return {'sameHref':sameHref,'diffHrefs':diffHrefs, 'domainRef' : domainRef , 'date':date,'article':str(article)}
+    return {'sameHref':list(sameHref),'diffHrefs':list(diffHrefs), 'domainRef' : list(domainRef) , 'date':date,'article':str(article)}
--- Python/fakenewsai.py
+++ Python/fakenewsai.py
@@ -3,31 +3,22 @@ from selenium.webdriver.common.keys import Keys
 from pyvirtualdisplay import Display
 import time
 
-def fakeNewsAI(domain):
-    def correct_url(url): 
-        if not url.startswith("http://") and not url.startswith("https://"):
-            url = "http://" + url
-        return url
-
-    def crawl_url(domain, url="www.fakenewsai.com"):
-        display = Display(visible=1, size=(1024, 768))
+def fakeNewsAI(domain, ff=True):
+    if(ff):
+        driver = webdriver.Firefox()
+        display = Display(visible=0, size=(1024, 768))
         display.start()
+    else:
+        driver=webdriver.Chrome('D:\\epfl\\Lauzhack\\StartHack2k18\\Python\\chromedriver.exe')
 
-        url = correct_url(url)
-        browser = webdriver.Firefox()
-        browser.get(url)
+    page = driver.get('http://www.fakenewsai.com/');
+    inpu = driver.find_element_by_tag_name('input')
+    inpu.clear()
+    inpu.send_keys(url)
+    driver.find_element_by_class_name('button').click()
 
-        inputUrl = browser.find_element_by_id("url")
-        inputUrl.clear()
-        inputUrl.send_keys(domain)
-        inputUrl.send_keys(Keys.RETURN)
-        time.sleep(2)
-        if(str(1) in browser.find_element_by_id("real").get_attribute("style")):
-            retvalue= "Probably not fake"
-        elif(str(1) in browser.find_element_by_id("fake").get_attribute("style")):
-            retvalue="Probably fake"
-        elif(str(1) in browser.find_element_by_id("error").get_attribute("style")):
-            retvalue="Error"
-        browser.quit()
-        return retvalue
-    return crawl_url(domain)
+    time.sleep(5)
+    for resM in driver.find_elements_by_class_name('results') :
+        if resM.get_attribute('style') == 'opacity: 1;' :
+            return resM.text
+    return "CRITICAL ERROR"
